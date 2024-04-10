602a3a3de8f628947c2838790770e6a281501ac1
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-04-10 12:18:02 +0530

    age

--- index.js
+++ index.js
@@ -1,4 +1,24 @@
 const express = require("express");
 const app = express();
 
+
+function isOldEnough(age){
+    if(age >= 14){
+        return true
+    }else{
+        return false;
+    }
+}
+app.get("/ride1",function(req,res){
+    //let entryAge = req.query.age;
+    if(isOldEnough(req.query.age)){
+        res.json({
+            msg:"you have successfully ridden ride1"
+        })
+    }else{
+        res.status(411).json({
+            msg:"sorry you are below the age"
+        })
+    }
+})
 app.listen(3000)
