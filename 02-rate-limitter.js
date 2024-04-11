9cf0ed7f08eae354ea828f66071e6a324f0f72e8
subhranshu sekher dhal <ssdhal98@gmail.com>
2024-04-11 12:24:32 +0530

    done

--- 02-rate-limitter.js
+++ 02-rate-limitter.js
@@ -1,6 +1,6 @@
 // const request = require('supertest');
 // const assert = require('assert');
-const express = require('express');
+const express = require("express");
 const app = express();
 // You have been given an express server which has a few endpoints.
 // Your task is to create a global middleware (app.use) which will
@@ -11,32 +11,33 @@ const app = express();
 // You have been given a numberOfRequestsForUser object to start off with which
 // clears every one second
 
-
 let numberOfRequestsForUser = {};
 
 setInterval(() => {
-    numberOfRequestsForUser = {};
-}, 1000)
+  numberOfRequestsForUser = {};
+}, 1000);
 
-function globalMiddleware(req,res,next){
+function globalMiddleware(req, res, next) {
   const userId = req.headers["user-id"];
-  if(numberOfRequestsForUser[userId]){
-    numberOfRequestsForUser++;
-    if(numberOfRequestsForUser[userId] > 5){
+  if (numberOfRequestsForUser[userId]) {
+    numberOfRequestsForUser = numberOfRequestsForUser + 1;
+    if (numberOfRequestsForUser[userId] > 5) {
       res.status(404).send("no entry");
-    }else{
-      next()
+    } else {
+      next();
     }
+  } else {
+    numberOfRequestsForUser[userId] = 1;
+    next();
   }
-  //numberOfRequestsForUser[req.headers["user-id"]]++
 }
 
-app.get('/user', function(req, res) {
-  res.status(200).json({ name: 'john' });
+app.get("/user", function (req, res) {
+  res.status(200).json({ name: "john" });
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post("/user", function (req, res) {
+  res.status(200).json({ msg: "created dummy user" });
 });
 
 module.exports = app;
